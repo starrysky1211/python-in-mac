833afb23683eebe9facba2477003bc10e5bf31fe
starrysky1211 <starry_sky_@outlook.com>
2017-02-28 16:21:34 +0800

    Trying to update num.py
    Want to let it have the ability to print a larger one, not only 0~9.

--- python-practice/print-the-num.py
+++ python-practice/print-the-num.py
@@ -1,9 +1,6 @@
 #\21天学通Python\
-b=1
-
-while(b):
-    a = input('a num between 0~9: ')
-    a = int(a)
+def printnum(num):
+    a = num
     if a < 0 | a > 10 :
         print ('invilid input!')
     elif a == 1 :
@@ -26,5 +23,22 @@ while(b):
         print('****','*  *','****','   *','****',sep = '\n')
     elif a == 0 :
         print('****','*  *','*  *','*  *','****',sep = '\n')
+def splitnum(num):
+    a = num
+    if a >10000 :
+        print ('invilid num')
+    else:
+        a1 = a%10
+        a2 = (a-a%10)%100/10
+        a3 = (a-a%100)%1000/100
+        a4 = (a-a%1000)%10000/1000
+        a5 = (a-a%10000)%100000/10000
+    return a1,int(a2),int(a3),int(a4),int(a5)
+
+b = 1
+while(b):
+    x = input('Please input an int num : ')
+    x = int(x)
+    print (splitnum(x))
     b = input('want to continue? y(1)/n(0)')
     b = int(b)
