46d16aa94bff622bff7a77446e73663354af16c0
starrysky1211 <starry_sky_@outlook.com>
2017-03-13 21:05:01 +0800

    ip_now upload to ftp//:101.6.58.142/

---
+++ python-practice/fileupload.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+from ftplib import FTP
+import time
+import tarfile
+import os
+
+# !/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+
+def ftp_connect(host, username, password):
+    ftp = FTP()
+    ftp.connect(host, 21)
+    ftp.login(username, password)
+    return ftp
+
+
+def ftp_upload(ftp, remotepath, localpath):
+    bufsize = 1024
+    fp = open(localpath, 'wb')
+    ftp.retrbinary('RETR'+remotepath, fp.write, bufsize)
+    ftp.set_debuglevel(0)
+    fp.close()
+
+
+if __name__=="__main__":
+    ftp = ftp_connect("101.6.58.142", "grad", "1003-2")
+    ftp_upload(ftp, "C:/Users/starrysky/Desktop/ip_now.txt", "\\101.6.58.142\grad\柳铮")
+
+    ftp.quit()
+
--- python-practice/tkinterpractice.py
+++ python-practice/tkinterpractice.py
@@ -4,22 +4,15 @@ from tkinter import *
 import time
 
 
-def get_time():
-
-    global time1
-    time2 = time.strftime('%H:%M:%S')
-    if time2 != time1:
-        time1 = time2
-        clock.config(text=time1)
-    clock.after(200, get_time)
+def print_time():
+    nt = time.CLOCK_REALTIME
+    print(nt)
 
 
 root = Tk()
-time1 = ' '
-clock = Label(root,
-              font=('times', 20, 'bold'),
-              bg='blue')
-clock.grid(row=0, column=1)
-get_time()
+#
+#
 
+#
+#
 root.mainloop()
