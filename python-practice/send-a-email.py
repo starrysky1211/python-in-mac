d912ca4c84aa5d1192824766234dd4c099c1a09c
starrysky1211 <starry_sky_@outlook.com>
2017-03-14 16:29:11 +0800

    email-program v.2.0 done with attachment

--- python-practice/send-a-email.py
+++ python-practice/send-a-email.py
@@ -1,21 +1,50 @@
+# -*- coding: UTF-8 -*-
 import smtplib
-import email.MIMEMultipart
-import email.MIMEText
-import os.path
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
-# 构造邮件
-msg = email.MIMEText('hello, send from Python! ', 'plain', 'utf-8')
 
-# 替换成发送邮箱和接收邮箱以及文件名
-From = "starry_sky_@outlook.com"
-To = "meliuzheng@foxmail.com"
-file_name = "file name"
-password = input("password: ")
-# 输入发送邮箱的服务器地址
-server = smtplib.SMTP("smtp-mail.outlook.com", 587)
-# 调试设定
-server.set_debuglevel(1)
-# 登陆服务器
-server.login(From, str(password))
-server.sendmail(From, To, msg.as_string())
-server.quit()
+mail_to_list = ['starry_sky_@outlook.com']
+mail_host = "smtp.163.com"
+mail_user = "15652826118"
+mail_pass = "sunny1231"
+mail_postfix = "163.com"
+
+
+def send_mail(to_list, sub, content):
+    me = "<" + mail_user + "@" + mail_postfix + ">"
+    msg = MIMEMultipart()
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = ";".join(to_list)
+
+    body = MIMEText(content, "plain", 'utf-8')
+    msg.attach(body)
+
+    with open('/Users/starrysky/GitHub/hexo/_config.yml', 'rb') as f:
+        mime = MIMEBase('file', 'yml', filename='_config.yml')
+
+        mime.add_header('Content-Disposition', 'attachment', filename='_config.yml')
+        mime.set_payload(f.read())
+        mime['Content-Type'] = 'application/octet-stream'
+
+        msg.attach(mime)
+
+    try:
+        server = smtplib.SMTP()
+        server.connect(mail_host)
+        server.login(mail_user, mail_pass)
+        server.sendmail(me, to_list, msg.as_string())
+        server.close()
+        return True
+    except Exception as e:
+        print (str(e))
+        return False
+
+
+for i in range(1):
+    if send_mail(mail_to_list, "text-mail", "有没有附件？"):
+        print "done! "
+    else:
+        print "failed! "
